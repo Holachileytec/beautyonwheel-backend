fae4fbda47a7cf7dd2e2629f6cca8006d7c7f6fa
ibrahim fatai <writetoppy_2014@yahoo.com>
2026-03-07 01:54:55 +0000

    update

--- routes/AdminRoute.js
+++ routes/AdminRoute.js
@@ -11,10 +11,10 @@ const {
   registerAdmin,
 } = require("../controller/AdminController.js");
 
-router.post("/login ", adminLogin);
+router.post("/login", adminLogin);
 router.post("/logout",adminLogout)
 router.post("/code", AdminPasscode);
-router.post("/admin/register", registerAdmin); // ← new route
+router.post("/register", registerAdmin); // ← new route
 router.get("/profile", auth, isAdmin, getAdmin);
 router.put("/profile", auth, isAdmin, adminUpdateProfile);
 
